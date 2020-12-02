1222e79d7382406add41e414c9eff2adfd926d74
clayjackson <clayjackson@CLAYJACKSON4B2A>
2020-12-01 22:13:31 -0500

    Day 1 Part 1 solution

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -9,7 +9,34 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            double magicNumber = 2020;
+
+            List<double> numbers = CreateArray(input);
+            numbers.Sort();
+
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                double matchingNumber = magicNumber - numbers[i];
+                if (numbers.IndexOf(matchingNumber) != -1)
+                {
+                    return Convert.ToString(matchingNumber * numbers[i]);
+                }
+            }
+
+            return "null";
+        }
+        
+        public List<double> CreateArray(string input)
+        {
+            List<double> num = new List<double>();
+
+            foreach (string line in input.Split("\n"))
+            {
+                num.Add(Convert.ToDouble(line));
+
+            }
+
+            return num;
         }
 
         public override string PartTwo(string input)
